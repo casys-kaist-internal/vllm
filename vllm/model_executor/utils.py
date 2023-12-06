2f116478978ebdc2afd570afe7baeda78248ea45
sjchoi <sjchoi@casys.kaist.ac.kr>
2023-12-06 11:49:41 +0900

    updating sps to new version

--- vllm/model_executor/utils.py
+++ vllm/model_executor/utils.py
@@ -4,19 +4,14 @@ from typing import Any, Dict, Optional
 
 import numpy as np
 import torch
-from vllm.model_executor.parallel_utils.parallel_state import ParallelState
-from vllm.model_executor.parallel_utils.tensor_parallel import model_parallel_cuda_manual_seed ## yh : parallel code 
 
 
-def set_random_seed(seed: int, parallel_state: ParallelState) -> None:
+def set_random_seed(seed: int) -> None:
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
-
-    if parallel_state.model_parallel_is_initialized():
-        model_parallel_cuda_manual_seed(seed, parallel_state)
 
 
 def set_weight_attrs(
