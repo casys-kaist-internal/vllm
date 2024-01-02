6ebdcd96c3cfbec7b15dc8bcab193a87e40d59c2
Sangjin Choi <sjchoi@casys.kaist.ac.kr>
2024-01-02 19:32:49 +0900

    profiling

--- vllm/model_executor/layers/sampler.py
+++ vllm/model_executor/layers/sampler.py
@@ -595,9 +595,9 @@ def _sps_sample(
         torch.rand_like(accept_prob) < accept_prob,
         torch.zeros_like(accept_prob), torch.ones_like(accept_prob))
 
-    accepted = torch.where(
-        torch.full_like(accept_prob, 0.5) < accept_prob,
-        torch.zeros_like(accept_prob), torch.ones_like(accept_prob))
+    # accepted = torch.where(
+    #     torch.full_like(accept_prob, 0.5) < accept_prob,
+    #     torch.zeros_like(accept_prob), torch.ones_like(accept_prob))
 
     # cumulative sum
     accepted.cumsum_(dim=1)
