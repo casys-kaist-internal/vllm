a662abfcdc208aaaf09e91f645bb0dffa64bbf48
Noppanat Wadlom <noppanat.wad@gmail.com>
2024-06-04 18:23:16 +0900

    implement async training

--- vllm/core/sps_draft_optim.py
+++ vllm/core/sps_draft_optim.py
@@ -1,8 +1,8 @@
 import datetime
 import os
 from abc import abstractmethod
-from threading import Thread
-from typing import List
+from threading import Thread, Semaphore
+from typing import List, Union
 
 import matplotlib.pyplot as plt
 import time
@@ -10,15 +10,17 @@ import os
 import numpy as np
 import pandas as pd
 import torch
+from sklearn.base import clone
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import ElasticNet
 from sklearn.pipeline import Pipeline
 from torch.cuda import nvtx
 
+from vllm.config import SpSConfig
 from vllm.sequence import Sequence, SequenceGroup, SequenceStatus
 
-PLOT_HEATMAP = True
+PLOT_HEATMAP = False
 DEFER_EXIT = False
 
 def defer_exit(delay: float):
@@ -46,16 +48,27 @@ class DraftSizeOptimizer:
 
 
 class BetaEMADraftSizeOptimizer(DraftSizeOptimizer):
-    def __init__(self, sps_config):
+    def __init__(self, sps_config: SpSConfig):
         self.sps_config = sps_config
         self.retrain_index = 0
-        self.retrain_period = 100000
-        self.history_size = 1000000
+        self.retrain_period = 10000
+        self.history_size = 100000
         self.num_bins = 20
         self.predictor = self._init_predictor()
         self.lookup_table = self._init_lookup_table()
         self.draft_history = {"beta_ema": [], "draft_prob": [], "accept_prob": []}
-        self.retrain = False
+        self.retrain = True
+        self.use_lookup_table = self.sps_config.use_lookup_table
+
+        if sps_config.use_async_training:
+            self.worker = Thread(target=self._train_predictor_async)
+            self.worker_sema = Semaphore(0)
+            self.worker.start()
+        else:
+            self.worker = None
+            self.worker_sema = None
+
+        self.initialize("/home/noppanat/workspace/vllm/benchmarks/predictor/lookup_table.csv") # TODO(noppanat): remove this
 
     # I think we should put this back to SpSConfig because SpSScheduler might need this. 
     def get_tile_size(self):
@@ -74,7 +87,7 @@ class BetaEMADraftSizeOptimizer(DraftSizeOptimizer):
 
         # Predict
         nvtx.range_push(f"predict {X[0][0]} {X[0][1]}")
-        if self.sps_config.use_lookup_table:
+        if self.use_lookup_table:
             accept_probs = np.clip(self._lookup_predict(X), 0, 1)
         else:
             accept_probs = np.clip(self.predictor.predict(X), 0, 1)
@@ -143,10 +156,11 @@ class BetaEMADraftSizeOptimizer(DraftSizeOptimizer):
         self.retrain_index += 1
         if self.retrain and self.retrain_index >= self.retrain_period:
             self.retrain_index = 0
-            nvtx.range_push("retrain predictor")
-            self._train_predictor()
-            nvtx.range_pop()
-            self._drop_draft_history()
+            print("[debug] (noppanat) time to retrain:", str(datetime.datetime.now()), flush=True)
+            if self.worker is None:
+                self._train_predictor_sync()
+            else:
+                self.worker_sema.release()
 
         self._update_drafted_accepted_df(seq)
 
@@ -196,6 +210,7 @@ class BetaEMADraftSizeOptimizer(DraftSizeOptimizer):
     
     def _lookup_predict(self, X):
         # Predict using the lookup table
+        X = np.array(X)
         beta_indices = (X[:, 0] * (self.num_bins - 1)).astype(int)
         draft_indices = (X[:, 1] * (self.num_bins - 1)).astype(int)
         accept_probs = self.lookup_table[beta_indices, draft_indices]
@@ -211,123 +226,29 @@ class BetaEMADraftSizeOptimizer(DraftSizeOptimizer):
         self.draft_history["draft_prob"].extend(draft_probs)
         self.draft_history["accept_prob"].extend(accept_probs)
 
-    def _train_predictor(self):
-        global DEFER_EXIT
-        if not DEFER_EXIT:
-            print("[debug] (noppanat) time:", str(datetime.datetime.now()), flush=True)
-            torch.cuda.cudart().cudaProfilerStart()
-            # Thread(target=defer_exit, args=(10,)).start()
-            DEFER_EXIT = True
-
-        nvtx.range_push("bin draft history")
-        binned_df = self._get_binned_draft_history_df()
-        nvtx.range_pop()
-
-        X = (
-            binned_df[["beta_ema", "draft_prob"]]
-            .apply(lambda x: pd.to_numeric(x, errors="coerce"))
-            .to_numpy()
-        )
-        y = binned_df["accept_prob"].to_numpy()
-
-        # Check if the input data is empty
-        if X.shape[0] == 0 or y.shape[0] == 0:
-            print("No data available for training the predictor. Skipping this training step.")
-            return
-
-        # Set the grid dimensions
-        grid_extent = [0, 1, 0, 1]
-
-        if PLOT_HEATMAP:
-            # Predict the "before" state
-            # Create a grid of (beta, draft) pairs using a Pandas DataFrame with feature names
-            beta_values = np.linspace(0, 1, self.num_bins)
-            draft_values = np.linspace(0, 1, self.num_bins)
-
-            # Generate pairs as a DataFrame
-            X_predict = [[beta, draft] for beta in beta_values for draft in draft_values]
-
-        # Check if the input data is empty
-        if X.shape[0] == 0 or y.shape[0] == 0:
-            print("No data available for training the predictor. Skipping this training step.")
-            return
-        
-        if self.sps_config.use_lookup_table:
-            # Fill the lookup table based on the specified aggregation type
-            self.lookup_table.fill(0)  # Reset the lookup table
-            beta_indices = (X[:, 0] * (self.num_bins - 1)).astype(int)
-            draft_indices = (X[:, 1] * (self.num_bins - 1)).astype(int)
-
-            if self.sps_config.predictor_agg_type == "mean":
-                np.add.at(self.lookup_table, (beta_indices, draft_indices), y)
-                counts = np.zeros_like(self.lookup_table)
-                np.add.at(counts, (beta_indices, draft_indices), 1)
-                self.lookup_table = np.divide(self.lookup_table, counts, where=counts != 0)
-            elif self.sps_config.predictor_agg_type == "median":
-                for i in range(self.num_bins):
-                    for j in range(self.num_bins):
-                        mask = (beta_indices == i) & (draft_indices == j)
-                        if mask.any():
-                            self.lookup_table[i, j] = np.median(y[mask])
-            print("Lookup table updated successfully.")
+    def _train_predictor_sync(self):
+        if self.use_lookup_table:
+            predictor = self.lookup_table
         else:
-            # Linear regression training
-            nvtx.range_push("fit predictor")
-            self.predictor.fit(X, y)
-            nvtx.range_pop()
+            predictor = self.predictor
+        
+        self._train_predictor(predictor)
 
-        if PLOT_HEATMAP:
+    def _train_predictor_async(self):
+        while self.worker_sema.acquire():
+            # Make a copy of the draft optimizer states.
             if self.use_lookup_table:
-                y_predict = self.lookup_table
+                predictor = np.zeros_like(self.lookup_table)
             else:
-                beta_values = np.linspace(0, 1, self.num_bins)
-                draft_values = np.linspace(0, 1, self.num_bins)
-                X_predict = [[beta, draft] for beta in beta_values for draft in draft_values]
-                y_predict = self.predictor.predict(X_predict).reshape(self.num_bins, self.num_bins)
+                predictor = clone(self.predictor)
+            
+            self._train_predictor(predictor)
 
-            # Create a matrix for real acceptance probability heatmap
-            pivot_df = binned_df.pivot(index="beta_ema", columns="draft_prob", values="accept_prob")
-
-            # Create the 2 subplots: Real, Predict
-            fig, axs = plt.subplots(1, 2, figsize=(24, 6), sharey=True)
-
-            # Plot "Real Data" (Acceptance Probability Heatmap)
-            axs[0].imshow(pivot_df, cmap='Accent', interpolation='nearest', origin='lower', extent=grid_extent)
-            axs[0].set_title('Real Acceptance Probability Heatmap')
-            axs[0].set_xlabel('Draft Probability')
-
-            # Plot "After" (Final Model Prediction)
-            axs[1].imshow(y_predict, cmap='Accent', interpolation='nearest', origin='lower', extent=grid_extent)
+            # Update the predictor
             if self.use_lookup_table:
-                axs[1].set_title('After (Lookup Table)')
+                self.lookup_table = predictor
             else:
-                axs[1].set_title('After (Final Model Prediction)')
-            axs[1].set_xlabel('Draft Probability')
-
-            if not self.use_lookup_table:
-                # Retrieve the final coefficients and intercept
-                feature_names = self.predictor.named_steps['poly'].get_feature_names_out()
-                linear_model =  self.predictor.named_steps['linear']
-
-                # Print the coef_ and intercept_ of the linear model
-                # print("Coefficients: ", linear_model.coef_)
-                # print("Intercept: ", linear_model.intercept_)
-
-                polynomial_function_string = create_polynomial_equation(linear_model, feature_names)
-                fig.text(0.5, 0.01, polynomial_function_string, ha='center', va='bottom', fontsize=10)
-
-            # Adjust the layout and add a colorbar
-            fig.colorbar(axs[0].images[0], ax=axs, orientation='vertical', pad=0.03)
-            fig.suptitle('Real vs. After Acceptance Probability Heatmaps')
-
-            # Create the directory for saving the plots if it doesn't exist
-            # os.makedirs('heatmap', exist_ok=True)
-            # current_time = time.strftime("%Y%m%d-%H%M%S")
-            # If path does not exist, create the directory
-            png_path = self.path.replace('.csv', '.png')
-            os.makedirs(os.path.dirname(png_path), exist_ok=True)
-            plt.savefig(png_path)
-            print(f"Plot saved to {png_path}.")
+                self.predictor = predictor
 
     def _get_binned_draft_history_df(self) -> pd.DataFrame:
         draft_accepted_df = pd.DataFrame(self.draft_history)
@@ -366,11 +287,11 @@ class BetaEMADraftSizeOptimizer(DraftSizeOptimizer):
     def initialize(self, path):
         self.path = path
         # if use_lookup_table then path should have lookup_table.csv
-        if self.sps_config.use_lookup_table:
+        if self.use_lookup_table:
             assert path.endswith("lookup_table.csv"), "Lookup table path should end with 'lookup_table.csv'"
 
         if os.path.exists(path):
-            if self.sps_config.use_lookup_table:
+            if self.use_lookup_table:
                 # Load the lookup table
                 self.lookup_table = pd.read_csv(path, index_col=0).values
                 print("Lookup table loaded successfully.")
@@ -405,7 +326,7 @@ class BetaEMADraftSizeOptimizer(DraftSizeOptimizer):
         self._init_draft_history()
 
     def save_predictor(self):
-        if self.sps_config.use_lookup_table:
+        if self.use_lookup_table:
             # Save the lookup table
             lookup_df = pd.DataFrame(self.lookup_table)
             
@@ -417,7 +338,7 @@ class BetaEMADraftSizeOptimizer(DraftSizeOptimizer):
             print(f"Lookup table saved to {self.path}.")
         else:
             # Retrain last time before saving
-            self._train_predictor()
+            self._train_predictor(self.predictor)
             
             # Extract the coefficients and intercept from the predictor
             predictor = self.predictor.named_steps['linear']
@@ -436,3 +357,131 @@ class BetaEMADraftSizeOptimizer(DraftSizeOptimizer):
 
             print(f"Predictor saved to {self.path}.")
             self.retrain = False
+
+    def _train_predictor(self, predictor: Union[Pipeline, np.ndarray]):
+        nvtx.range_push("retrain predictor")
+
+        global DEFER_EXIT
+        if not DEFER_EXIT:
+            print("[debug] (noppanat) time:", str(datetime.datetime.now()), flush=True)
+            torch.cuda.cudart().cudaProfilerStart()
+            Thread(target=defer_exit, args=(10,)).start()
+            DEFER_EXIT = True
+        
+        if self.use_lookup_table:
+            assert isinstance(predictor, np.ndarray)
+        else:
+            assert isinstance(predictor, Pipeline)
+
+        nvtx.range_push("bin draft history")
+        binned_df = self._get_binned_draft_history_df()
+        nvtx.range_pop()
+
+        X = (
+            binned_df[["beta_ema", "draft_prob"]]
+            .apply(lambda x: pd.to_numeric(x, errors="coerce"))
+            .to_numpy()
+        )
+        y = binned_df["accept_prob"].to_numpy()
+
+        # Check if the input data is empty
+        if X.shape[0] == 0 or y.shape[0] == 0:
+            print("No data available for training the predictor. Skipping this training step.")
+            return
+
+        # Set the grid dimensions
+        grid_extent = [0, 1, 0, 1]
+
+        if PLOT_HEATMAP:
+            # Predict the "before" state
+            # Create a grid of (beta, draft) pairs using a Pandas DataFrame with feature names
+            beta_values = np.linspace(0, 1, self.num_bins)
+            draft_values = np.linspace(0, 1, self.num_bins)
+
+            # Generate pairs as a DataFrame
+            X_predict = [[beta, draft] for beta in beta_values for draft in draft_values]
+
+        # Check if the input data is empty
+        if X.shape[0] == 0 or y.shape[0] == 0:
+            print("No data available for training the predictor. Skipping this training step.")
+            return
+        
+        if self.use_lookup_table:
+            # Fill the lookup table based on the specified aggregation type
+            predictor.fill(0)  # Reset the lookup table
+            beta_indices = (X[:, 0] * (self.num_bins - 1)).astype(int)
+            draft_indices = (X[:, 1] * (self.num_bins - 1)).astype(int)
+
+            if self.sps_config.predictor_agg_type == "mean":
+                np.add.at(predictor, (beta_indices, draft_indices), y)
+                counts = np.zeros_like(predictor)
+                np.add.at(counts, (beta_indices, draft_indices), 1)
+                predictor = np.divide(predictor, counts, where=counts != 0)
+            elif self.sps_config.predictor_agg_type == "median":
+                for i in range(self.num_bins):
+                    for j in range(self.num_bins):
+                        mask = (beta_indices == i) & (draft_indices == j)
+                        if mask.any():
+                            predictor[i, j] = np.median(y[mask])
+            print("Lookup table updated successfully.")
+        else:
+            # Linear regression training
+            nvtx.range_push("fit predictor")
+            predictor.fit(X, y)
+            nvtx.range_pop()
+
+        if PLOT_HEATMAP:
+            if self.use_lookup_table:
+                y_predict = predictor
+            else:
+                beta_values = np.linspace(0, 1, self.num_bins)
+                draft_values = np.linspace(0, 1, self.num_bins)
+                X_predict = [[beta, draft] for beta in beta_values for draft in draft_values]
+                y_predict = predictor.predict(X_predict).reshape(self.num_bins, self.num_bins)
+
+            # Create a matrix for real acceptance probability heatmap
+            pivot_df = binned_df.pivot(index="beta_ema", columns="draft_prob", values="accept_prob")
+
+            # Create the 2 subplots: Real, Predict
+            fig, axs = plt.subplots(1, 2, figsize=(24, 6), sharey=True)
+
+            # Plot "Real Data" (Acceptance Probability Heatmap)
+            axs[0].imshow(pivot_df, cmap='Accent', interpolation='nearest', origin='lower', extent=grid_extent)
+            axs[0].set_title('Real Acceptance Probability Heatmap')
+            axs[0].set_xlabel('Draft Probability')
+
+            # Plot "After" (Final Model Prediction)
+            axs[1].imshow(y_predict, cmap='Accent', interpolation='nearest', origin='lower', extent=grid_extent)
+            if self.use_lookup_table:
+                axs[1].set_title('After (Lookup Table)')
+            else:
+                axs[1].set_title('After (Final Model Prediction)')
+            axs[1].set_xlabel('Draft Probability')
+
+            if not self.use_lookup_table:
+                # Retrieve the final coefficients and intercept
+                feature_names = predictor.named_steps['poly'].get_feature_names_out()
+                linear_model =  predictor.named_steps['linear']
+
+                # Print the coef_ and intercept_ of the linear model
+                # print("Coefficients: ", linear_model.coef_)
+                # print("Intercept: ", linear_model.intercept_)
+
+                polynomial_function_string = create_polynomial_equation(linear_model, feature_names)
+                fig.text(0.5, 0.01, polynomial_function_string, ha='center', va='bottom', fontsize=10)
+
+            # Adjust the layout and add a colorbar
+            fig.colorbar(axs[0].images[0], ax=axs, orientation='vertical', pad=0.03)
+            fig.suptitle('Real vs. After Acceptance Probability Heatmaps')
+
+            # Create the directory for saving the plots if it doesn't exist
+            # os.makedirs('heatmap', exist_ok=True)
+            # current_time = time.strftime("%Y%m%d-%H%M%S")
+            # If path does not exist, create the directory
+            png_path = self.path.replace('.csv', '.png')
+            os.makedirs(os.path.dirname(png_path), exist_ok=True)
+            plt.savefig(png_path)
+            print(f"Plot saved to {png_path}.")
+
+        nvtx.range_pop()
+        self._drop_draft_history()
